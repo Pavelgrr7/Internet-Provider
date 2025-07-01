97279ca95351aa46b64ef15e43d679bf8b77de4c
Pavel Ryzhenkov <mpaasha4@gmail.com>
2025-07-01 23:23:41 +0700

    Initial commit

---
+++ src/main/java/com/pavelryzh/provider/InternetProvApplication.java
@@ -0,0 +1,13 @@
+package com.pavelryzh.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetProvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternetProvApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pavelryzh/provider/InternetProvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pavelryzh.provider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetProvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
